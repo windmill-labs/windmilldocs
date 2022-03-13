bc938bd9f7bebd03fdc9daccff223f584e949c15
Ruben Fiszel <ruben@rubenfiszel.com>
2022-03-13 17:09:02 +0100

    iterate

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -11,18 +11,19 @@ const faqs = [
 		answer:
 			(<p>Businesses need to automate internal workflows (for example, renewing a user license key
 				and sending an email) to be efficient and scalable. In many companies, developers write
-					quick and dirty scripts to automate repetitive tasks, but these scripts never make it to
-					production and never get widespread adoption. <br /> <br />
-					Developers face a choice : keep the script for themselves - creating bottlenecks for the
-					business - or spend a lot of time to make it usable by other employees - a big waste of
-					developer productivity. What if you could immediately turn these quick and dirty scripts
-					into internal apps with user interfaces, workflows and permissioning? <br />
-					<br />
-					What if automation could flow from developers to the entire company? Windmill is a
-					developer-first platform that quickly turns scripts into user-friendly internal apps,
+				quick and dirty scripts to automate repetitive tasks, but these scripts never make it to
+				production and never get widespread adoption. <br /> <br />
+				Developers face a choice : keep the script for themselves - creating bottlenecks for the
+				business - or spend a lot of time to make it usable by other employees - a big waste of
+				developer productivity. What if you could immediately turn these quick and dirty scripts
+				into internal apps with user interfaces, workflows and permissioning? <br />
+				<br />
+				What if automation could flow from developers to the entire company? Windmill is a
+				developer-first platform that quickly turns scripts into user-friendly internal apps,
 				enabling companies to automate and scale internal workflows.</p>)
 	},
-	{ question: "Is Windmill a no-code platform?",
+	{
+		question: "Is Windmill a no-code platform?",
 		answer: (<p>					No. We believe that writing code is the most efficient way to build internal apps, for
 			several reasons:
 			<ul className="list-disc mx-12">
@@ -38,7 +39,8 @@ const faqs = [
 				</li>
 			</ul>
 			However, there are part of building internal apps that are painful and not worth a
-			developer's time: building UI, workflows, permissions. Windmill removes those time sinks.</p>)}
+			developer's time: building UI, workflows, permissions. Windmill removes those time sinks.</p>)
+	}
 ]
 
 function classNames(...classes)
@@ -49,35 +51,33 @@ function classNames(...classes)
 export default function FAQ()
 {
 	return (
-		<div className="bg-white">
-			<div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-				<div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
-					<h2 className="text-center text-3xl font-bold text-gray-900 sm:text-4xl">Frequently asked questions</h2>
-					<dl className="mt-6 space-y-6 divide-y divide-gray-200">
-						{faqs.map((faq) => (
-							<Disclosure as="div" key={faq.question} className="pt-6">
-								{({ open }) => (
-									<>
-										<dt className="text-lg">
-											<Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
-												<span className="font-medium text-gray-900">{faq.question}</span>
-												<span className="ml-6 h-7 flex items-center">
-													<ChevronDownIcon
-														className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
-														aria-hidden="true"
-													/>
-												</span>
-											</Disclosure.Button>
-										</dt>
-										<Disclosure.Panel as="dd" className="mt-6 pr-12">
-											<p className="font-light text-lg">{faq.answer}</p>
-										</Disclosure.Panel>
-									</>
-								)}
-							</Disclosure>
-						))}
-					</dl>
-				</div>
+		<div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+			<div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+				<h2 className="text-center text-3xl font-bold text-gray-900 sm:text-4xl">Frequently asked questions</h2>
+				<dl className="mt-6 space-y-6 divide-y divide-gray-200">
+					{faqs.map((faq) => (
+						<Disclosure as="div" key={faq.question} className="pt-6">
+							{({ open }) => (
+								<>
+									<dt className="text-lg">
+										<Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+											<span className="font-medium text-gray-900">{faq.question}</span>
+											<span className="ml-6 h-7 flex items-center">
+												<ChevronDownIcon
+													className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+													aria-hidden="true"
+												/>
+											</span>
+										</Disclosure.Button>
+									</dt>
+									<Disclosure.Panel as="dd" className="mt-6 pr-12">
+										<p className="font-light text-lg">{faq.answer}</p>
+									</Disclosure.Panel>
+								</>
+							)}
+						</Disclosure>
+					))}
+				</dl>
 			</div>
 		</div>
 	)
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,22 +12,22 @@ function HomepageHeader()
 {
 
 	return (
-		<div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-5">
+		<div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-5" style={{ "min-height": "600px" }}>
 			<div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
 				<div>
-					<div className="mt-20">
+					<div className="mt-20 lg:mt-40">
 						<div className="mt-6 sm:max-w-xl">
-							<h1 className="text-3xl tracking-tight font-bold text-gray-900 sm:text-5xl md:text-6xl font-mono">
+							<h1 className="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl md:text-4xl font-mono">
 								<span className="block xl:inline">Company-wide <span className="block text-blue-600 xl:inline">apps</span> and <span className="block text-blue-600 xl:inline">automations</span>{' '}from minimal python <span className="block text-blue-600 xl:inline">scripts</span></span>
 							</h1>
 						</div>
-						<div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+						<div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center lg:mt-20">
 							<div className="shadow">
 								<a
 									href="https://alpha.windmill.dev/user/login"
 									className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
 								>
-									Try it, it's free
+									Sign up
 								</a>
 							</div>
 							<div className="mt-3 shadow sm:mt-0 sm:ml-10">
@@ -53,7 +53,8 @@ function HomepageHeader()
 			</div>
 
 			<div className="sm:mx-auto sm:max-w-3xl sm:px-6">
-				<div className="pb-12 sm:relative sm:mt-12 sm:pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+				<div className="pb-12 sm:relative sm:mt-12 sm:pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+				>
 					<div className="hidden sm:block">
 						<div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
 						<svg
@@ -78,7 +79,8 @@ function HomepageHeader()
 							<rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
 						</svg>
 					</div>
-					<div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
+					<div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12"
+					>
 						<img
 							className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
 							src="/img/editor.png"
@@ -87,7 +89,7 @@ function HomepageHeader()
 					</div>
 				</div>
 			</div>
-		</div>
+		</div >
 
 	);
 	{/* // <div className="md:grid md:grid-cols-3 -mt-14 pt-14">
@@ -131,11 +133,65 @@ function HomepageHeader()
 
 }
 
+const roadmapItems = [
+	{ title: "Slack Approval flows", description: "Ask approval to a user or group through Slack or email before a script is ran", when: "End of March" },
+	{
+		title: "Self-hostable workers", description: `Self-hostable workers will allow to host the workers on-prem. 
+		The secrets being encrypted assymetrically, this will allow even sensitive environments to run windmill`, when: "End of April"
+	},
+	{
+		title: "(Work)Flows", description: `(Work)Flows will enable anyone, even less technical users to compose and chain scripts together. 
+		An output to input transform tool will have a dedicated UI in-between scripts to keep scripts as modular as possible`, when: "End of March"
+	},
+	{
+		title: "Advanced Slack integration", description: `It is currently possible to trigger one script per workspace from Slack. 
+	An advanced integration will allow for any script to be chosen directly from slack, and the input to be set directly from slack using the block UI API.`, when: "Enf of Q2"
+	},
+	{
+		title: "Advanced Google workspace/gmail/drive/docs/spreadsheet integration", description: `A dedicated oauth integration will allow to connect a workspace to a Google Workspace as easily as it is currently possible for a Slack workspace. The wmill integrated python client will
+	integrate some utility function to make working with gmail/drive/docs/spreadsheet in a convenience fashion.`, when: "End of Q2"
+	},
+	{ title: "Advanced Airtable integration", description: `Same as the "Advanced Google workspace integration" but for Airtable`, when: "End of Q2" },
+	{ title: "Advanced Notion integration", description: `Same as the "Advanced Google workspace integration" but for Notion`, when: "End of Q2" },
+	{ title: "Support JS/TS/Ruby", description: "Support other programming languages than Python", when: "End of Q2 and based on feedback" }
+
+]
+function Roadmap()
+{
+
+	return (
+		<div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+			<div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+				<h2 className="text-center text-3xl font-bold text-gray-900 sm:text-4xl">Roadmap</h2>
+				<ul role="list" class="mt-6 divide-y divide-gray-200">
+					{roadmapItems.map((x) =>
+						<li class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+							<div class="flex justify-between space-x-3">
+								<div class="min-w-0 flex-1">
+									<span class="absolute inset-0" aria-hidden="true"></span>
+									<p class="text-sm font-medium text-gray-900 truncate">{x.title}</p>
+								</div>
+								<span class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{x.when}</span>
+							</div>
+							<div class="mt-1">
+								<p class="line-clamp-2 text-sm text-gray-600">{x.description}</p>
+							</div>
+						</li>
+					)}
+
+				</ul>
+			</div>
+		</div>
+
+
+	);
+}
+
 export default function Home()
 {
 	return (
 		<Layout
-			title="windmill.dev landing page"
+			title="Home"
 		>
 			<main>
 				<HomepageHeader />
@@ -144,6 +200,7 @@ export default function Home()
 				<div className='mx-auto max-w-screen-lg homepage'>
 					<Pricing />
 					<FAQ />
+					<Roadmap />
 				</div>
 				<div className='mb-40'></div>
 			</main>
