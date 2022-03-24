0da8785e86cc387ed99e669ebd345c19636f2519
Ruben Fiszel <ruben@rubenfiszel.com>
2022-03-24 18:37:00 +0100

    better header

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -29,8 +29,6 @@ export default class VideoPlayer extends React.Component
 					onClose={() => this.setState({ isOpen: false })}
 				/>
 				<div className='w-full h-full cursor-pointer' onClick={this.openModal}>
-					<div className="hover:opacity-30 opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-5xl text-blue-600 font-light font-mono">
-						<span className='border-4 p-2 border-solid border-blue-600'>Watch demo</span></div>
 					{this.props.children}</div>
 			</React.Fragment >
 		);
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,25 +12,25 @@ function HomepageHeader()
 {
 
 	return (
-		<div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-5" style={{ "min-height": "600px" }}>
-			<div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
-				<div>
-					<div className="mt-20 lg:mt-40">
-						<div className="mt-6 sm:max-w-xl">
+		<main class="mx-auto max-w-7xl px-4 sm:mt-20 sm:px-6 ">
+			<div class="lg:grid lg:grid-cols-12 lg:gap-8">
+				<div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+					<div >
+						<div className="mt-20 sm:max-w-xl">
 							<h1 className="text-3xl tracking-tight font-bold text-gray-900 sm:text-4xl md:text-4xl font-mono">
 								<span className="block xl:inline">Company-wide <span className="block text-blue-600 xl:inline">apps</span> and <span className="block text-blue-600 xl:inline">automations</span>{' '}from minimal python <span className="block text-blue-600 xl:inline">scripts</span></span>
 							</h1>
 						</div>
-						<div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center lg:mt-20">
+						<div className="mt-5  max-w-md mx-auto sm:flex sm:justify-center lg:mt-10">
 							<div className="shadow">
 								<a
 									href="https://alpha.windmill.dev/user/login"
-									className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+									className="w-full rounded flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
 								>
 									Sign up
 								</a>
 							</div>
-							<div className="mt-3 shadow sm:mt-0 sm:ml-10">
+							<div className="mt-3 shadow sm:mt-0 rounded sm:ml-10">
 								<a
 									href="/docs/intro"
 									className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
@@ -43,102 +43,103 @@ function HomepageHeader()
 							<a
 								target='_blank'
 								href="https://forms.gle/9ANRUru75nqrKT6C9"
-								className="mx-20 shadow flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-50 "
+								className="mx-20 shadow flex items-center rounded justify-center px-8 py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-50 "
 							>
 								Get a demo
 							</a>
 						</div>
 					</div>
 				</div>
-			</div>
-
-			<div className="sm:mx-auto sm:max-w-3xl sm:px-6">
-				<div className="pb-12 sm:relative sm:mt-12 sm:pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
-				>
-					<div className="hidden sm:block">
-						<div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
-						<svg
-							className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
-							width={404}
-							height={392}
-							fill="none"
-							viewBox="0 0 404 392"
-						>
-							<defs>
-								<pattern
-									id="837c3e70-6c3a-44e6-8854-cc48c737b659"
-									x={0}
-									y={0}
-									width={20}
-									height={20}
-									patternUnits="userSpaceOnUse"
-								>
-									<rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-								</pattern>
-							</defs>
-							<rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
-						</svg>
-					</div>
-
-					<div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12"
-					>
-						<BrowserOnly>
-							{() =>
-								<VideoPlayer>
-									<img
-										className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
-										src="/img/editor.png"
-										alt=""
-									/>
-								</VideoPlayer>
-							}
+				<div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+					<svg class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden" width="640" height="784" fill="none" viewBox="0 0 640 784" aria-hidden="true">
+						<defs>
+							<pattern id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+								<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+							</pattern>
+						</defs>
+						<rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+						<rect x="118" width="404" height="784" fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)" />
+					</svg>
+					<div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-2xl">
+						<BrowserOnly>{() =>
+							<VideoPlayer>
+								<button type="button" class="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+									<span class="sr-only">Watch our video to learn more</span>
+									<img class="w-full" src="/img/editor.png" alt="" />
+									<div class="absolute inset-0 w-full h-full flex items-center justify-center" aria-hidden="true">
+										<svg class="h-20 w-20 text-blue-600" fill="currentColor" viewBox="0 0 84 84">
+											<circle opacity="0.5" cx="42" cy="42" r="42" fill="gray" />
+											<path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+										</svg>
+									</div>
+								</button>
+							</VideoPlayer>
+						}
 						</BrowserOnly>
 					</div>
 				</div>
 			</div>
-		</div >
+		</main>
 
 	);
-	{/* // <div className="md:grid md:grid-cols-3 -mt-14 pt-14">
-		// 	<div className="flex bottom-0 bg-blue-500 text-white text-center py-6 md:col-span-1">
-		// 		<div className="flex flex-col items-center m-auto px-6">
-		// 			<p className="text-md text-gray-100  text-left mb-5">
-		// 				Turn scripts into a production-grade internal apps that all the team can use.
-		// 			</p>
-		// 			<div className="flex flex-row items-center m-auto mt-6 ">
-		// 				<a
-		// 					className="button button--secondary  mx-2 px-2"
-		// 					href="https://alpha.windmill.dev"
-		// 				>
-		// 					Try the open alpha
-		// 				</a>
-		// 				<a
-		// 					className="button button--secondary px-2"
-		// 					href="mailto:alpha@windmill.dev?subject=Request%20demo%20access"
-		// 				>
-		// 					Contact us
-		// 				</a>
-		// 				<BrowserOnly>
-		// 					{() => 
-		// 					<VideoPlayer>
-		// 						<button class="button text-white hover:text-white hover:underline px-2 mx-3">
-		// 							Watch demo
-		// 						</button>
-		// 					</VideoPlayer>
-		// 					}
-		// 				</BrowserOnly>
-
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// 	<div className="flex pt-4 md:col-span-2 px-1 sm:px-3 h-full">
-		// 		<div className="rounded-3xl m-auto object-fit py-10 w-full md:w-10/12">
-		// 			<Slideshow />
-		// 		</div>
-		// 	</div>
-		// </div> */}
-
 }
+
+
+
+// <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-5" style={{ "min-height": "600px" }}>
+// 	<div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+// 		<div>
+
+// 		</div>
+// 	</div>
+
+// 	<div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+// 		<div className="pb-12 sm:relative sm:mt-12 sm:pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+// 		>
+// 			<div className="hidden sm:block">
+// 				<div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
+// 				<svg
+// 					className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
+// 					width={404}
+// 					height={392}
+// 					fill="none"
+// 					viewBox="0 0 404 392"
+// 				>
+// 					<defs>
+// 						<pattern
+// 							id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+// 							x={0}
+// 							y={0}
+// 							width={20}
+// 							height={20}
+// 							patternUnits="userSpaceOnUse"
+// 						>
+// 							<rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+// 						</pattern>
+// 					</defs>
+// 					<rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
+// 				</svg>
+// 			</div>
+
+// 			<div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12"
+// 			>
+
+// 				<BrowserOnly>
+// 					{() =>
+// 						<VideoPlayer>
+// 							<img
+// 								className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+// 								src="/img/editor.png"
+// 								alt=""
+// 							/>
+// 						</VideoPlayer>
+// 					}
+// 				</BrowserOnly>
+// 			</div>
+// 		</div>
+// 	</div>
+// </div >
+
 
 const roadmapItems = [
 	{ title: "Slack Approval module for flows", description: "Ask approval to a user or group through Slack or email as a blocking step of a flow", when: "End of March" },
