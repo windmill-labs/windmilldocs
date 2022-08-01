b42125ef106acd9ba2d2458b93b1d3ac54fb99df
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-01 19:46:34 +0200

    use flows image instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ function HomepageHeader()
 							<VideoPlayer>
 								<button type="button" class="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 									<span class="sr-only">Windmill demo</span>
-									<img class="w-full" src="/img/editor.png" alt="" />
+									<img class="w-full" src="/img/flows.png" alt="" />
 									<div class="absolute inset-0 w-full h-full flex items-center justify-center" aria-hidden="true">
 										<svg class="h-20 w-20 text-blue-600" fill="currentColor" viewBox="0 0 84 84">
 											<circle opacity="0.5" cx="42" cy="42" r="42" fill="gray" />
