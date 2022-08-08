1dde2bcaf9770813c04697fa63934d2793aaa33b
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-08 12:43:59 +0200

    add csp

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,14 @@ const config = {
 			},
 		],
 	],
+
+	themeConfig: {
+		metadata: [{
+			"http-equiv": "Content-Security-Policy", "Content-Security-Policy:": "default-src 'self' *.windmill.dev; script-src static.cloudflareinsights.com; connect-src cloudflareinsights.com;"
+		}],
+		// This would become <meta name="keywords" content="cooking, blog"> in the generated HTML
+	},
+
 	plugins: [
 		async function myPlugin(context, options)
 		{
@@ -134,7 +142,8 @@ const config = {
 				disableSwitch: true,
 				respectPrefersColorScheme: false,
 			},
-		})
+		}),
+
 };
 
 module.exports = config;
