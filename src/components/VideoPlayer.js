620aa012ae75806c3994751a3a1eae4ed722abb3
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-18 16:15:26 +0200

    update demo

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -25,7 +25,7 @@ export default class VideoPlayer extends React.Component
 				<ModalVideo
 					channel="youtube"
 					isOpen={this.state.isOpen}
-					videoId="rfnM70y52X8"
+					videoId="dfBSLfrV0-c"
 					onClose={() => this.setState({ isOpen: false })}
 				/>
 				<div className='w-full h-full cursor-pointer' onClick={this.openModal}>
