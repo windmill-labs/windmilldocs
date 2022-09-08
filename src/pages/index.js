50d2ef16e1d361dfb6985579b7d22962a12deff3
Ruben Fiszel <ruben@rubenfiszel.com>
2022-09-08 18:11:36 +0200

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -185,7 +185,7 @@ function HomepageHeader() {
 						type="submit"
 						class="mb-8 py-2 px-3 text-sm font-medium text-center text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 rounded-lg mr-2 "
 					>
-						View all exemples on WindmillHub
+						View all examples on WindmillHub
 					</button>
 				</form>
 				<TabsW></TabsW>
