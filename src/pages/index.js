7640a57e9026aa9b486f67918df9ec015f084832
malomarrec <malo.marrec@gmail.com>
2022-10-13 11:02:10 +0200

    turn feature list into story

--- src/pages/index.js
+++ src/pages/index.js
@@ -177,7 +177,7 @@ function HomepageHeader() {
 				</div>
 			</div>
 			<div className="w-full flex items-center flex-col text-center">
-				<h1 class=" subpixel-antialiased	 mb-4 text-3xl font-bold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl ">
+				<h1 class="section-title">
 					What can you build with{' '}
 					<span class="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-blue-500">
 						Windmill
@@ -203,61 +203,6 @@ function HomepageHeader() {
 		</main>
 	);
 }
-
-// <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-5" style={{ "min-height": "600px" }}>
-// 	<div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
-// 		<div>
-
-// 		</div>
-// 	</div>
-
-// 	<div className="sm:mx-auto sm:max-w-3xl sm:px-6">
-// 		<div className="pb-12 sm:relative sm:mt-12 sm:pb-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
-// 		>
-// 			<div className="hidden sm:block">
-// 				<div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
-// 				<svg
-// 					className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
-// 					width={404}
-// 					height={392}
-// 					fill="none"
-// 					viewBox="0 0 404 392"
-// 				>
-// 					<defs>
-// 						<pattern
-// 							id="837c3e70-6c3a-44e6-8854-cc48c737b659"
-// 							x={0}
-// 							y={0}
-// 							width={20}
-// 							height={20}
-// 							patternUnits="userSpaceOnUse"
-// 						>
-// 							<rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-// 						</pattern>
-// 					</defs>
-// 					<rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
-// 				</svg>
-// 			</div>
-
-// 			<div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12"
-// 			>
-
-// 				<BrowserOnly>
-// 					{() =>
-// 						<VideoPlayer>
-// 							<img
-// 								className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
-// 								src="/img/editor.png"
-// 								alt=""
-// 							/>
-// 						</VideoPlayer>
-// 					}
-// 				</BrowserOnly>
-// 			</div>
-// 		</div>
-// 	</div>
-// </div >
-
 const roadmapItems = [
 	{
 		title: 'Embeddable Apps',
@@ -369,8 +314,8 @@ export default function Home() {
 		<Layout title="Home">
 			<main>
 				<HomepageHeader />
-				<WhoisItFor />
 				<HomepageFeatures />
+				<WhoisItFor />
 				<div className="mx-auto max-w-screen-lg homepage">
 					<Pricing />
 					<FAQ />
