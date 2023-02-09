20f9bc2a6430e95ac258b876ef68f4fecf39a14e
hcourdent <122811744+hcourdent@users.noreply.github.com>
2023-02-09 12:00:48 +0100

    Update FlowSection.jsx

--- src/landing/FlowSection.jsx
+++ src/landing/FlowSection.jsx
@@ -308,11 +308,13 @@ export default function FlowSection() {
 						Explore more flows on the Hub
 						<ExternalLink className="ml-2 h-5" />
 					</a>
+
+
 					<a
 						href="https://app.windmill.dev/user/login"
 						type="button"
 						target="_blank"
-						className="inline-flex items-center rounded-md border border-transparent bg-green-400 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 !no-underline hover:text-white"
+						className="inline-flex items-center rounded-md border border-transparent bg-green-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 !no-underline hover:text-white"
 					>
 						Try building flows yourself
 						<ExternalLink className="ml-2 h-5" />
