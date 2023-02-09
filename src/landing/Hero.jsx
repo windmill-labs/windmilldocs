ccc981e2343c3f2a6b24b47909f9116787763f99
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-09 18:11:25 +0100

    remove invicible

--- src/landing/Hero.jsx
+++ src/landing/Hero.jsx
@@ -42,7 +42,7 @@ export default function Hero() {
 						Star
 					</GitHubButton>
 					<h1 className="mt-4 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-						Turn scripts into invincible workflows and UIs in minutes
+						Turn scripts into workflows and UIs in minutes
 					</h1>
 					<p className="mt-6 text-lg leading-8 text-gray-600">
 						Easily create internal apps and invincible workflows with code only where it matters.
