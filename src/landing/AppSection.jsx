0fc087b91bc4602fb386e5e17661152b7442f864
hcourdent <122811744+hcourdent@users.noreply.github.com>
2023-02-09 14:08:07 +0100

    Update AppSection.jsx

--- src/landing/AppSection.jsx
+++ src/landing/AppSection.jsx
@@ -198,11 +198,11 @@ export default function AppSection() {
 					</a>
 
 					<a
-						href="https://app.windmill.dev/user/login"
+						href="https://app.windmill.dev/apps/add?nodraft=true"
 						type="button"
 						target="_blank"
-						className="ml-4 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-100 px-4 py-2 text-base font-medium text-blue-600 shadow-sm hover:bg-blue-200 hover:text-blue-800 !no-underline transition-all"					>
-						Try building scripts
+						className="ml-4 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-600 shadow-sm hover:bg-blue-200 hover:text-blue-800 !no-underline transition-all"					>
+						Try building apps
 						<ExternalLink className="ml-2 h-5" />
 					</a>
 				</div>
