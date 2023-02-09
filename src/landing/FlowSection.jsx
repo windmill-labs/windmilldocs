41a070ae8f4b8f760e933504a90503596ba364d8
hcourdent <122811744+hcourdent@users.noreply.github.com>
2023-02-09 14:27:40 +0100

    Update FlowSection.jsx

--- src/landing/FlowSection.jsx
+++ src/landing/FlowSection.jsx
@@ -313,7 +313,7 @@ export default function FlowSection() {
 						href="https://app.windmill.dev/flows/add?nodraft=true"
 						type="button"
 						target="_blank"
-						className="ml-4 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-600 shadow-sm hover:bg-blue-200 hover:text-blue-800 !no-underline transition-all"					>
+						className="text-sm font-semibold leading-7 text-white !no-underline"
 						Try building flows
 						<ExternalLink className="ml-2 h-5" />
 					</a>
