6ef5e7b3d9d3c7c439a9af01a6576c0e6dd3f81e
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-09 18:12:01 +0100

    remove invicible

--- src/landing/Hero.jsx
+++ src/landing/Hero.jsx
@@ -33,13 +33,13 @@ export default function Hero() {
 			<div className="mx-auto max-w-7xl px-6 pt-10 gap-y-8 pb-24 sm:pb-32 lg:flex lg:py-20 lg:px-8 mt-4">
 				<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
 					<GitHubButton
+						alt="Stars"
 						href="https://github.com/windmill-labs/windmill"
 						data-icon="octicon-star"
 						data-show-count="true"
 						data-size="large"
 						aria-label="Star windmill-labs/windmill on GitHub"
 					>
-						Star
 					</GitHubButton>
 					<h1 className="mt-4 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
 						Turn scripts into workflows and UIs in minutes
