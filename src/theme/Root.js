6f549d058e41a75e68889a262bd63b5105adecea
Faton Ramadani <faton.ramadani14@gmail.com>
2023-06-22 13:09:31 +0200

    Merge main

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,11 +1,13 @@
-import { LoaderIcon } from 'lucide-react';
 import React, { useEffect } from 'react';
 
 export default function Root({ children }) {
 	let [loaded, setLoaded] = React.useState(false);
 
 	useEffect(() => {
-		document.documentElement.setAttribute('data-theme', 'light');
+		const url = window.location.href;
+		if (!url.includes('/docs/')) {
+			document.documentElement.setAttribute('data-theme', 'light');
+		}
 		setLoaded(true);
 	});
 
