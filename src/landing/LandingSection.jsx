90955d61da2e3c10c36e09b9502dbf049c3ec701
Faton Ramadani <faton.ramadani14@gmail.com>
2023-07-04 07:55:12 +0200

    Fix blur overflow

--- src/landing/LandingSection.jsx
+++ src/landing/LandingSection.jsx
@@ -5,7 +5,7 @@ export default function LandingSection({
 	bgClass = 'landing-section-gradient-background'
 }) {
 	return (
-		<div className={`w-full h-full py-16 overflow-x-clip ${bgClass}`}>
+		<div className={`w-full h-full py-16 overflow-x-hidden ${bgClass}`}>
 			<div className="max-w-7xl px-6 lg:px-8 mx-auto flex justify-center items-center h-full ">
 				{children}
 			</div>
