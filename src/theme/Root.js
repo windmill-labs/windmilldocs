c6aebfde96d00f75bf734098fc1bdd06c86a62b0
Faton Ramadani <faton.ramadani14@gmail.com>
2023-07-11 11:33:43 +0200

    fix(frontend): Fix chrome anchors (#241)

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useLocation } from '@docusaurus/router';
 
 export default function Root({ children }) {
 	// Temporary fix for:
 	// https://github.com/facebook/docusaurus/issues/7986
 	// https://www.joshwcomeau.com/react/the-perils-of-rehydration/#the-solution-9
-	const [hasMounted, setHasMounted] = React.useState(false);
+
+	const location = useLocation();
+	const isDocs = location.pathname.includes('/docs/');
+	const [hasMounted, setHasMounted] = React.useState(isDocs);
 
 	React.useEffect(() => {
 		setHasMounted(true);
