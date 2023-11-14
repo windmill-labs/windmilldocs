bc5b7619b108e5b08e9ae3ab60e6111c0451a47a
Guillaume Bouvignies <guillaume@windmill.dev>
2023-11-14 12:04:03 +0100

    refactor: Add bar charts to benchmarks page (#374)
    * docs: Windmill Airflow Temporal benchmarks
    
    * Fix broken links
    
    * Update numbers from linux-based benchmarks
    
    * Try to fix tables
    
    * Add Prefect
    
    * Add Prefect 2
    
    * Add bar charts to benchmarks
    
    * Remove old index.md

---
+++ src/components/TaskDurationBarChart.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+
+export default function BarChart({
+	title,
+	labels,
+	rawData,
+	maintainAspectRatio = true,
+}) {
+	ChartJS.register(
+		CategoryScale,
+		LinearScale,
+		BarElement,
+		Title,
+		Tooltip,
+		Legend
+	);
+
+	const standby_col = 'rgba(188, 212, 252, 1)';
+	const task_running_col = 'rgba(59, 130, 246, 1)';
+
+	let datasets_data = [];
+
+	let c = 0;
+	for (let i = 0; i < rawData[0].length; i++) {
+		for(let j = 0; j < rawData[0][i].length; j++) {
+			datasets_data.push({
+				axis: 'y',
+				label: c % 2 === 0 ? 'waiting' : 'task_' + Math.floor(c / 2).toString(),
+				data: [],
+				fill: false,
+				backgroundColor: [
+					c % 2 === 0 ? standby_col : task_running_col,
+				],
+				borderColor: [
+					'rgb(0, 0, 0, 0)',
+				],
+				borderWidth: 1
+			});
+			c += 1;
+		}
+	}
+
+	for (let i = 0; i < rawData.length; i++) {
+		c = 0;
+		for (let j = 0; j < rawData[i][0].length; j++) {
+			datasets_data[c].data.push(rawData[i][0][j]);
+			c += 1;
+			datasets_data[c].data.push(rawData[i][1][j]);
+			c += 1;
+		}
+	}
+
+	const data = {
+		labels: labels,
+		datasets: datasets_data,
+	};
+
+	const options = {
+		indexAxis: 'y' as const,
+		elements: {
+			bar: {
+				borderWidth: 2,
+			},
+		},
+		scales: {
+			x: {
+				stacked: true
+			},
+			y: {
+				stacked: true
+			}
+		},
+		responsive: true,
+		maintainAspectRatio: maintainAspectRatio,
+		plugins: {
+			legend: {
+				display: false,
+			},
+			title: {
+				display: true,
+				text: title,
+			},
+		},
+	};
+
+	return <Bar options={options} data={data} />;
+}
