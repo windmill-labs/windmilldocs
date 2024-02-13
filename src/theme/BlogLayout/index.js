6b81092abdb2525bc446dd0ecce924f207fbb864
Faton Ramadani <faton.ramadani14@gmail.com>
2024-02-13 11:47:28 +0100

    Restore swizzled components

---
+++ src/theme/BlogLayout/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import BlogSidebar from '@theme/BlogSidebar';
+import { twMerge } from 'tailwind-merge';
+
+export default function BlogLayout(props) {
+	const { sidebar, toc, children, ...layoutProps } = props;
+	const hasSidebar = sidebar && sidebar.items.length > 0;
+	const isBlogPostPageList = Boolean(toc);
+	const isChangelog = window.location.pathname.includes('/changelog');
+
+	return (
+		<Layout {...layoutProps}>
+			{!isBlogPostPageList && (
+				<div className={twMerge('w-full mx-auto', isChangelog ? 'max-w-6xl' : 'max-w-7xl ')}>
+					<div className="">
+						<div className="relative pt-16 pb-8 overflow-hidden">
+							<h1 className="tracking-tight container leading-tight text-left font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-900 dark:from-blue-300 dark:to-blue-500">
+								{isChangelog ? 'Changelog' : 'Blog'}
+							</h1>
+							<p className="mt-4 text-lg leading-8 container">
+								{isChangelog
+									? "See what's new with Windmill."
+									: 'Discover the latest news, updates, and articles from the team and community.'}
+							</p>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 1024 1024"
+								className="absolute -top-80 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 -translate-y-1/2"
+								aria-hidden="true"
+							>
+								<circle
+									cx={512}
+									cy={512}
+									r={512}
+									fill="url(#827591b1-ce8c-4110-b064-7cb85a0b121745)"
+									fillOpacity="0.7"
+								/>
+								<defs>
+									<radialGradient
+										id="827591b1-ce8c-4110-b064-7cb85a0b121745"
+										cx={0}
+										cy={0}
+										r={1}
+										gradientUnits="userSpaceOnUse"
+										gradientTransform="translate(512 512) rotate(90) scale(512)"
+									>
+										<stop stopColor="#3b82f6" />
+										<stop offset={1} stopColor="#3b82f6" stopOpacity={0} />
+									</radialGradient>
+								</defs>
+							</svg>
+						</div>
+					</div>
+				</div>
+			)}
+			<div
+				className={twMerge(
+					' w-full mx-auto margin-vert--lg bg-transparent',
+					isChangelog ? 'max-w-6xl  px-4' : 'max-w-7xl container'
+				)}
+			>
+				<div className="row">
+					<BlogSidebar sidebar={sidebar} />
+					{toc && <div className="col col--2"></div>}
+
+					<main
+						className={clsx(
+							'col',
+							{
+								'col--7': hasSidebar,
+								'w-full mx-auto': !hasSidebar && !isBlogPostPageList,
+								'max-w-3xl mx-auto': !hasSidebar && isBlogPostPageList
+							},
+							isChangelog ? 'mt-0' : 'mt-12'
+						)}
+						itemScope
+						itemType="http://schema.org/Blog"
+					>
+						{children}
+					</main>
+					{toc && <div className="col col--2">{toc}</div>}
+				</div>
+			</div>
+		</Layout>
+	);
+}
---
+++ src/theme/BlogPostItem/Container/index.js
@@ -0,0 +1,202 @@
+import React from 'react';
+import { useBaseUrlUtils } from '@docusaurus/useBaseUrl';
+import { useBlogPost } from '@docusaurus/theme-common/internal';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import { ArrowRight, FileText } from 'lucide-react';
+import { Archive, Blocks, Sparkles } from 'lucide-react';
+import Head from '@docusaurus/Head';
+import { twMerge } from 'tailwind-merge';
+
+export default function BlogPostItemContainer({ children, className }) {
+	const { frontMatter, assets, isBlogPostPage, metadata } = useBlogPost();
+	const { withBaseUrl } = useBaseUrlUtils();
+	const image = assets.image ?? frontMatter.image;
+
+	const isChangelog = window.location.pathname.includes('/changelog');
+
+	const defaultTitle = 'Changelog | Windmill';
+	const defaultDescription = "See what's new with Windmill.";
+
+	const isFeaturePage = window.location.pathname.includes('/changelog/') && frontMatter.slug;
+
+	const pageTitle = isFeaturePage ? metadata.title : defaultTitle;
+	const pageDescription = isFeaturePage ? metadata.description : defaultDescription;
+
+	if (isChangelog) {
+		return (
+			<div className="w-full">
+				<Head>
+					<title>{pageTitle}</title>
+					<meta name="title" content={metadata.title} />
+					<meta property="og:title" content={metadata.title} />
+					<meta property="og:description" content={pageDescription} />
+					<meta property="og:image" content={image} />
+					<meta property="site_name" content="Windmill" />
+					<meta property="og:type" content="changelog" />
+					<meta property="og:url" content={window.location.href} />
+				</Head>
+				<div className="flex flex-row gap-2 items-center mb-4">
+					<h3 className="text-xl font-semibold">
+						<a
+							href={`/changelog/${frontMatter.slug}`}
+							className="text-gray-900 dark:text-white hover:text-blue-600"
+						>
+							{metadata.title}
+						</a>
+					</h3>
+					{metadata?.tags.map((tag, index) => (
+						<span
+							key={tag + index}
+							className={
+								'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium ' +
+								(tag.label === 'App Editor'
+									? 'bg-orange-100 text-orange-800'
+									: tag.label === 'Flow'
+									? 'bg-emerald-100 text-emerald-800'
+									: tag.label === 'Enterprise & Cloud'
+									? 'bg-gray-50 text-blue-900'
+									: 'bg-blue-100 text-blue-800')
+							}
+						>
+							{tag.label === 'Enterprise & Cloud' ? (
+								<a href="/pricing" className="text-blue-900 hover:text-blue-700">
+									{tag.label}
+								</a>
+							) : (
+								tag.label
+							)}
+						</span>
+					))}
+					<span
+						className={twMerge(
+							'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium',
+							'bg-gray-100 text-gray-800'
+						)}
+					>
+						{frontMatter.version}
+					</span>
+				</div>
+				<div className="mb-6">
+					<div className="flex items-center">
+						<p className="text-base dark:text-gray-300 text-gray-600 mr-3">
+							{new Date(metadata.date).toLocaleDateString()} |{' '}
+						</p>
+						{frontMatter.docs && (
+							<button
+								className="hover:bg-gray-50 shadow-sm ring-1 ring-inset ring-gray-300 dark:hover:bg-slate-800 px-2.5 py-1 rounded-xl text-xs text-gray-600 dark:text-gray-200"
+								onClick={() => (window.location.href = frontMatter.docs)}
+							>
+								<FileText
+									className="inline mr-2"
+									aria-hidden="true"
+									style={{ width: '14px', height: '14px' }}
+								/>
+								<span className="align-middle">Docs</span>
+							</button>
+						)}
+					</div>
+				</div>
+				<img
+					className="rounded-lg shadow-lg border h-96 w-full object-cover my-8"
+					src={image}
+					alt=""
+				/>
+				<p className="text-base dark:text-gray-200 text-gray-600">{metadata.description}</p>
+
+				{frontMatter?.improvements?.length > 0 && (
+					<div className="mt-6">
+						<h4 className="text-lg font-medium text-gray-900 dark:text-white flex flex-row gap-1 items-center">
+							<Sparkles className="inline-block w-6 h-6 mr-2" />
+							Improvements
+						</h4>
+						<ul className="mt-4 text-base dark:text-gray-200 text-gray-600 pl-8 flex flex-col gap-2">
+							{frontMatter?.improvements.map((improvement, idx) => (
+								<li key={idx + 'improvements'} className="list-disc">
+									{improvement}
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+				{frontMatter?.features?.length > 0 && (
+					<div className="mt-6">
+						<h4 className="text-lg font-medium text-gray-900 dark:text-white flex flex-row gap-1 items-center">
+							<Blocks className="inline-block w-6 h-6 mr-2" />
+							New Features
+						</h4>
+						<ul className="mt-4 text-base dark:text-gray-200 text-gray-600 pl-8 flex flex-col gap-2">
+							{frontMatter?.features.map((newFeature, idx) => (
+								<li key={idx + 'features'} className="list-disc">
+									{newFeature}
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+				{frontMatter?.deprecations?.length > 0 && (
+					<div className="mt-6">
+						<h4 className="text-lg font-medium text-gray-900 dark:text-white flex flex-row gap-1 items-center">
+							<Archive className="inline-block w-6 h-6 mr-2" />
+							Deprecations
+						</h4>
+						<ul className="mt-4 text-base dark:text-gray-200 text-gray-600 pl-8 flex flex-col gap-2">
+							{frontMatter?.deprecations.map((deprecation, idx) => (
+								<li key={idx + 'deprecations'} className="list-disc">
+									{deprecation}
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
+		);
+	}
+	return (
+		<article
+			className={clsx(className, {
+				'border p-2 rounded-2xl shadow-md hover:border-blue-400 hover:shadow-lg transition-all !mb-4 dark:border-gray-700 dark:hover:border-blue-400':
+					!isBlogPostPage
+			})}
+			itemProp="blogPost"
+			itemScope
+			itemType="http://schema.org/BlogPosting"
+		>
+			{image && <meta itemProp="image" content={(withBaseUrl(image), { absolute: true })} />}
+			{!isBlogPostPage ? (
+				<Link itemProp="url" to={metadata.permalink} className="!no-underline">
+					<img src={image} className="h-64 w-full rounded-2xl object-cover" />
+
+					<div className="p-4 flex flex-col h-64 overflow-auto">
+						<div className="text-xs font-semibold flex justify-between items-center">
+							<div class="flex flex-col">
+								{metadata.authors.map((author, index) => (
+									<span>
+										{index === 0 ? 'by ' : 'and '}
+										{author.name}
+									</span>
+								))}
+							</div>
+
+							<span>{Math.ceil(metadata.readingTime)} min read</span>
+						</div>
+
+						<h2 className="text-lg leading-6 font-semibold mt-4 mb-2 text-gray-900 dark:text-white">
+							{metadata.title}
+						</h2>
+
+						<p className="text-gray-600 text-sm h-max dark:text-gray-400">
+							{metadata.description ?? frontMatter.description}
+						</p>
+					</div>
+					<div className="flex flex-row gap-2 text-blue-400 font-semibold items-center p-4">
+						Read more
+						<ArrowRight size={16} className="opacity-50" />
+					</div>
+				</Link>
+			) : (
+				children
+			)}
+		</article>
+	);
+}
