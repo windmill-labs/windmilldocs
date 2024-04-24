000c2f66aae65bec5ee17cad2c30c4516fcf8b55
Henri Courdent <122811744+hcourdent@users.noreply.github.com>
2024-04-24 12:00:44 +0200

    End orphan pages & Bash scripts arguments (#594)
    * End orphan pages & Bash scripts arguments
    
    * Missing description
    
    * Fix fix description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -193,35 +193,6 @@ const config = {
 				defaultMode: 'dark',
 				disableSwitch: false,
 				respectPrefersColorScheme: true
-			},
-			algolia: {
-				// The application ID provided by Algolia
-				appId: '3Q3AONZ2W8',
-
-				// Public API key: it is safe to commit it
-				apiKey: '22ac914215bd02f11fcafa7ef9a9d1bf',
-
-				indexName: 'windmill',
-
-				// Optional: see doc section below
-				contextualSearch: true,
-
-				// Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-				externalUrlRegex: 'windmill\\.dev|www.windmill\\.dev',
-
-				// // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-				// replaceSearchResultPathname: {
-				// 	from: '/docs/', // or as RegExp: /\/docs\//
-				// 	to: '/'
-				// },
-
-				// Optional: Algolia search parameters
-				searchParameters: {},
-
-				// Optional: path for search page that enabled by default (`false` to disable it)
-				searchPagePath: 'search'
-
-				//... other Algolia params
 			}
 		})
 };
