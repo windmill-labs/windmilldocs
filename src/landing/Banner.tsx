3fcc17dec66536c03c9e27d5eba64c5c2a3b2b23
Henri Courdent <122811744+hcourdent@users.noreply.github.com>
2024-11-20 11:00:44 +0100

    New pricing banner (#763)

--- src/landing/Banner.tsx
+++ src/landing/Banner.tsx
@@ -5,11 +5,10 @@ export default function Banner() {
 	return (
 		<div className="flex items-center gap-x-6 bg-blue-700 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
 			<a
-				href="/docs/misc/guides/airplane"
+				href="/pricing"
 				className="text-sm leading-6 !text-white flex flex-row gap-2 items-center"
 			>
-				<strong className="font-semibold">Migration from Airplane to Windmill made easy</strong>
-				See docs <ArrowRight size={12} />
+				<strong className="font-semibold">New pricing now available</strong>
 			</a>
 			<div className="flex flex-1 justify-end"></div>
 		</div>
