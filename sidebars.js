378f75a0b8e13e6bdc6cfe9dac72d4193f20242c
dieriba <t.dieriba@gmail.com>
2025-02-27 17:24:06 +0100

    update docs

--- sidebars.js
+++ sidebars.js
@@ -155,6 +155,11 @@ const sidebars = {
 							type: 'doc',
 							id: 'core_concepts/sqs_triggers/index',
 							label: 'SQS'
+						},
+						{
+							type: 'doc',
+							id: 'core_concepts/mqtt_triggers/index',
+							label: 'MQTT'
 						}
 					]
 				},
