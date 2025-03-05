e855c18bb0eba76f7a1cc00093f1689a734748f5
pyranota <92104930+pyranota@users.noreply.github.com>
2025-03-05 11:24:36 +0300

    update flake (#851)
    * add flakes
    
    * add git to flake

--- flake.nix
+++ flake.nix
@@ -2,29 +2,39 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      buildInputs = with pkgs; [
-        # dev libxml2.dev xmlsec.dev libxslt.dev libtool
-        nodejs
-      ];
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs =
-          buildInputs
-          ++ (with pkgs; [
-            git
-          ]);
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ nodejs git ];
+          shellHook = ''
+            echo ""
+            echo "1. wm-setup"
+            echo "2. wm-dev"
+            echo ""
+          '';
+
+          packages = [
+            (pkgs.writeScriptBin "wm-setup" ''
+              ${pkgs.nodejs}/bin/npm i --legacy-peer-deps
+            '')
+            # Start dev server
+            (pkgs.writeScriptBin "wm-dev" ''
+              ${pkgs.nodejs}/bin/npm start
+            '')
+            # TODO:
+            # (pkgs.writeScriptBin "wm-build" ''
+            #   ${pkgs.nodejs}/bin/npm run build
+            # '')
+            # (pkgs.writeScriptBin "wm-check" "")
+          ];
+        };
+      });
 }
