e9beadc920bb19725c7d66a3c5c8099f26b4f568
Guilhem <guilhemlemouel@gmail.com>
2025-09-03 18:03:25 +0100

    Add horizontal padding for the pricing table

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -531,7 +531,7 @@ export default function Pricing() {
 				</div>
 				<div
 					className={classNames(
-						'isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none w-full',
+						'isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none w-full px-6',
 						{
 							'lg:grid-cols-3': frequency.value === 'cloud',
 							'lg:grid-cols-2': frequency.value === 'selfhost',
