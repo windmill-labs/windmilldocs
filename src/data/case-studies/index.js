35491905d770f1bfe852dd3b90354c924efd8868
tristantr <tristan.use@gmail.com>
2025-12-12 14:25:38 +0100

    Add Athena Intelligence case study

--- src/data/case-studies/index.js
+++ src/data/case-studies/index.js
@@ -1,9 +1,11 @@
 import { pantherLabsCaseStudy } from './panther-labs';
 import { cfaInstituteCaseStudy } from './cfa-institute';
 import { zoomCaseStudy } from './zoom';
+import { athenaIntelligenceCaseStudy } from './athena-intelligence';
 
 export const caseStudies = [
 	pantherLabsCaseStudy,
 	cfaInstituteCaseStudy,
-	zoomCaseStudy
+	zoomCaseStudy,
+	athenaIntelligenceCaseStudy
 ];
