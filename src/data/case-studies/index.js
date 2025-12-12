b0a7b047c941af863c6f9cbc5f47cb976d3cbdbd
tristantr <tristan.use@gmail.com>
2025-12-12 17:02:29 +0100

    Add Twilio case study

--- src/data/case-studies/index.js
+++ src/data/case-studies/index.js
@@ -3,11 +3,13 @@ import { cfaInstituteCaseStudy } from './cfa-institute';
 import { zoomCaseStudy } from './zoom';
 import { athenaIntelligenceCaseStudy } from './athena-intelligence';
 import { axiansCaseStudy } from './axians';
+import { twilioCaseStudy } from './twilio';
 
 export const caseStudies = [
 	pantherLabsCaseStudy,
 	cfaInstituteCaseStudy,
 	zoomCaseStudy,
 	athenaIntelligenceCaseStudy,
-	axiansCaseStudy
+	axiansCaseStudy,
+	twilioCaseStudy
 ];
