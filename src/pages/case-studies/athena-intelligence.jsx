2f6f06f8827ae85b356de1f604df6a1a256442d5
tristantr <tristan.use@gmail.com>
2025-12-15 17:09:15 +0100

    Create CaseStudyLayout component

--- src/pages/case-studies/athena-intelligence.jsx
+++ src/pages/case-studies/athena-intelligence.jsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import Footer from '../../landing/Footer';
-import LandingHeader from '../../landing/LandingHeader';
-import Head from '@docusaurus/Head';
-import RadialBlur from '../../landing/RadialBlur';
-import LayoutProvider from '@theme/Layout/Provider';
+import CaseStudyLayout from '../../components/case-studies/CaseStudyLayout';
 import AthenaIntelligenceContent, { frontMatter } from './athena-intelligence-content.mdx';
 
 export default function AthenaIntelligenceCaseStudyPage() {
-	return (
-		<LayoutProvider>
-			<main className="relative min-h-screen w-full overflow-x-hidden">
-				<LandingHeader />
-				<Head>
-					<title>{frontMatter.title} | Windmill Case Studies</title>
-					<meta name="title" content={frontMatter.title} />
-					<meta name="description" content={frontMatter.description} />
-					<link rel="icon" href="/img/logo.svg" />
-				</Head>
-				<>
-					<RadialBlur />
-					<AthenaIntelligenceContent title={frontMatter.title} description={frontMatter.description} />
-				</>
-				<Footer />
-			</main>
-		</LayoutProvider>
-	);
+	return <CaseStudyLayout Content={AthenaIntelligenceContent} frontMatter={frontMatter} />;
 }
