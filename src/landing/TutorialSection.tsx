90343ce887ca439dfa87b64e68b95585dbe2f188
tristantr <tristan.use@gmail.com>
2025-12-22 18:25:09 +0100

    Add benchmarks animation to deploy at scale card

--- src/landing/TutorialSection.tsx
+++ src/landing/TutorialSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	Activity,
 	GitCompareArrows,
@@ -7,12 +7,19 @@ import {
 } from 'lucide-react';
 import { Lottie } from './LightFeatureCard';
 // @ts-ignore
-import deployAtScale from '/illustrations/deploy_at_scale.json';
-// @ts-ignore
 import devfriendly from '/illustrations/devfriendly.json';
+import { BenchmarkVisualization } from '../components/BenchmarkVisualization';
 import { ArrowLongDownIcon } from '@heroicons/react/20/solid';
+import { useColorMode } from '@docusaurus/theme-common';
+import classNames from 'classnames';
+import { Switch } from '@headlessui/react';
 
 export default function TutorialSection() {
+	const [chart, setChart] = React.useState<'short' | 'long'>(undefined);
+
+	useEffect(() => {
+		setChart('short');
+	}, []);
 	const features = [
 		{
 			title: 'Build',
@@ -35,7 +42,7 @@ export default function TutorialSection() {
 			description: 'Deploy with ease on our infrastructure or your own infrastructure, on bare VMs with docker-compose, ecs, or large Kubernetes clusters with up to 1000 workers and even remote agents.',
 			icon: Server,
 			href: '/docs/advanced/deploy_to_prod',
-			lottieData: deployAtScale
+			useBenchmark: true
 		},
 		{
 			title: 'Monitor',
@@ -53,6 +60,7 @@ export default function TutorialSection() {
 	);
 
 	const FeatureCard = ({ feature, index }) => {
+		const { colorMode } = useColorMode();
 		const Icon = feature.icon;
 		const ContentWrapper = feature.href ? 'a' : 'div';
 		const wrapperProps = feature.href
@@ -85,7 +93,86 @@ export default function TutorialSection() {
 						</div>
 					</ContentWrapper>
 					<div className="col-span-3">
-						{feature.lottieData ? (
+						{feature.useBenchmark ? (
+							<div className="flex flex-col w-full gap-4">
+								<div className="flex flex-row gap-2 items-center transition-all">
+									<span className={classNames('font-light text-sm text-gray-900 dark:text-white')}>
+										10 long running tasks
+									</span>
+									<Switch
+										checked={chart === 'short'}
+										title="Switch between short and long running tasks"
+										onChange={() => {
+											setChart(chart === 'long' ? 'short' : 'long');
+										}}
+										className={`${
+											chart === 'short'
+												? 'bg-blue-500 dark:bg-blue-900'
+												: 'bg-gray-200 dark:bg-gray-800'
+										}
+          relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+									>
+										<span
+											aria-hidden="true"
+											className={`${chart === 'short' ? 'translate-x-6' : 'translate-x-0'}
+            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white  shadow-lg ring-0 transition duration-200 ease-in-out`}
+										/>
+									</Switch>
+									<span className={classNames('font-light text-sm text-gray-900 dark:text-white')}>
+										40 lightweight tasks
+									</span>
+					</div>
+								<div
+									className={classNames(
+										colorMode === 'dark' ? 'bg-black' : 'bg-gray-50',
+										'w-full p-8 bg-opacity-40 rounded-xl benchmark-chart-container'
+									)}
+									data-theme={colorMode}
+								>
+									<div className="grid">
+										{chart === 'short' ? (
+											<div>
+												<BenchmarkVisualization
+													usecase="fibonacci_40_10"
+													language="python"
+													engines={[
+														'airflow',
+														'kestra',
+														'prefect',
+														'temporal',
+														'windmill',
+														'windmill_dedicated'
+													]}
+													workers={1}
+													title="40 lightweight tasks comparison (animation time speed 20x)"
+													maintainAspectRatio={false}
+													shouldAnimate={true}
+												/>
+				</div>
+										) : (
+											<div>
+												<BenchmarkVisualization
+													usecase="fibonacci_10_33"
+													language="python"
+													engines={[
+														'airflow',
+														'kestra',
+														'prefect',
+														'temporal',
+														'windmill',
+														'windmill_dedicated'
+													]}
+													workers={1}
+													title="10 long running tasks comparison (animation time speed 20x)"
+													maintainAspectRatio={false}
+													shouldAnimate={true}
+												/>
+				</div>
+										)}
+						</div>
+						</div>
+						</div>
+						) : feature.lottieData ? (
 							<Lottie lottieData={feature.lottieData} autoplay loop />
 						) : (
 						<div className="rounded-lg overflow-hidden h-full w-full flex flex-col justify-end">
