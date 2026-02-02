61223c267eee24371ce85dc9077ccbc6db287e78
tristantr <tristan.use@gmail.com>
2026-02-02 14:31:14 +0100

    Improve Axians wording

--- src/data/case-studies/index.js
+++ src/data/case-studies/index.js
@@ -1,17 +1,17 @@
 import { cfaInstituteCaseStudy } from './cfa-institute';
 import { athenaIntelligenceCaseStudy } from './athena-intelligence';
 import { pantherLabsCaseStudy } from './panther-labs';
+import { axiansCaseStudy } from './axians';
 
 // Private case studies (uncomment when approved):
 // import { zoomCaseStudy } from './zoom';
-// import { axiansCaseStudy } from './axians';
 // import { twilioCaseStudy } from './twilio';
 
 export const caseStudies = [
 	cfaInstituteCaseStudy,
 	athenaIntelligenceCaseStudy,
-	pantherLabsCaseStudy
+	pantherLabsCaseStudy,
+	axiansCaseStudy
 	// zoomCaseStudy,
-	// axiansCaseStudy,
 	// twilioCaseStudy
 ];
