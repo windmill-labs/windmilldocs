e09cc72b3f3a9ad2b07dec962fe8fd191277fafc
tristantr <tristan.use@gmail.com>
2026-03-10 16:14:58 +0100

    Update workflow editor

--- src/pages/product/flow-editor.jsx
+++ src/pages/product/flow-editor.jsx
@@ -3,6 +3,6 @@ import ProductPageLayout from '../../components/products/ProductPageLayout';
 import Content, { frontMatter } from './workflow-editor-content.mdx';
 import { workflowEditorProduct } from '../../data/products/workflow-editor';
 
-export default function WorkflowEditorPage() {
+export default function flowEditorPage() {
 	return <ProductPageLayout Content={Content} frontMatter={frontMatter} productData={workflowEditorProduct} />;
 }
