ea43a2593ab7eba6da93c3a137fce8eecf894d64
tristantr <tristan.use@gmail.com>
2026-03-11 11:28:40 +0100

    Workers product page

---
+++ src/data/products/workers.js
@@ -0,0 +1,7 @@
+export const workersProduct = {
+	slug: 'workers',
+	name: 'Workers',
+	headline: 'Isolated workers that pull from a shared queue',
+	description: 'How do Windmill workers work? Route workloads by tag, scale horizontally, and deploy workers anywhere.',
+	link: '/product/workers',
+};
--- src/pages/product/scale.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ProductPageLayout from '../../components/products/ProductPageLayout';
-import Content, { frontMatter } from './scale-content.mdx';
-import { scaleProduct } from '../../data/products/scale';
-
-export default function ScalePage() {
-	return <ProductPageLayout Content={Content} frontMatter={frontMatter} productData={scaleProduct} />;
-}
