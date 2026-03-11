62ab9d6495b9ce9ac2400c5430db8e0a1f1bc010
tristantr <tristan.use@gmail.com>
2026-03-12 00:16:09 +0100

    nits

--- src/data/products/triggers.js
+++ src/data/products/triggers.js
@@ -1,7 +1,7 @@
 export const triggersProduct = {
 	slug: 'triggers',
 	name: 'Triggers',
-	headline: '16+ ways to trigger scripts and flows',
+	headline: 'Built in triggers for your scripts and flows',
 	description: 'How do I trigger scripts and flows in Windmill? Schedules, webhooks, Kafka, Postgres CDC, WebSockets, emails, and more.',
 	link: '/product/triggers',
 };
