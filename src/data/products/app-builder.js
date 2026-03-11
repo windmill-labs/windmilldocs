15622e8de610bab7b3c31c58a1177ac36c5726bd
tristantr <tristan.use@gmail.com>
2026-03-11 10:16:29 +0100

    Improve wording

--- src/data/products/app-builder.js
+++ src/data/products/app-builder.js
@@ -1,7 +1,7 @@
-export const frontendBuilderProduct = {
-	slug: 'frontend-builder',
-	name: 'Frontend builder',
+export const appBuilderProduct = {
+	slug: 'app-builder',
+	name: 'App builder',
 	headline: 'Full-code frontends connected to your backend',
 	description: 'Build custom UIs in React or Svelte with an auto-generated, type-safe API to backend runnables in 20+ languages.',
-	link: '/product/frontend-builder',
+	link: '/product/app-builder',
 };
