8ca7d8ce919afccaa17066717aa08c0c320b6ba3
tristantr <tristan.use@gmail.com>
2026-03-12 11:20:18 +0100

    Improve CTA on bottom of each Product page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Hero from '../landing/Hero';
 import Footer from '../landing/Footer';
-import LandingSection from '../landing/LandingSection';
-import CallToAction from '../landing/CallToAction';
+import ProductCTA from '../components/products/ProductCTA';
 import SeoHead from '../components/SeoHead';
 import HeroExample from '../landing/HeroExample';
 import LandingHeader from '../landing/LandingHeader';
@@ -88,9 +87,7 @@ function HomepageHeader() {
 			<CorePrinciple />
 			<TestimonialsSection />
 			<HeroExample />
-			<LandingSection bgClass="py-0">
-				<CallToAction />
-			</LandingSection>
+			<ProductCTA />
 			<Footer />
 		</>
 	);
