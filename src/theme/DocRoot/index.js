29f8f9b3a6f3f1a871bf169df4fa93007366d455
tristantr <tristan.use@gmail.com>
2026-03-30 18:40:49 +0200

    Add banner to docs

--- src/theme/DocRoot/index.js
+++ src/theme/DocRoot/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DocRoot from '@theme-original/DocRoot';
 import Footer from '../../landing/Footer';
+import Banner from '../../landing/Banner';
 
 export default function DocRootWrapper(props) {
 	return (
 		<>
+			<Banner />
 			<DocRoot {...props} />
 			<div className="">
 				<Footer />
