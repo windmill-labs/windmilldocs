0755bdbc6afad78b065e6493d450571b7f388826
tristantr <tristan.use@gmail.com>
2026-04-01 14:30:08 +0200

    Nit

--- src/data/use-cases/scripts.js
+++ src/data/use-cases/scripts.js
@@ -5,5 +5,6 @@ export const scriptsUseCase = {
 	description: 'Write scripts in TypeScript, Python, Go, Bash, SQL and more. Trigger them from webhooks, schedules, queues or the auto-generated UI.',
 	icon: 'CodeBracketIcon',
 	link: '/use-cases/scripts',
+	image: 'https://www.windmill.dev/img/money-pages/scripts-preview.webp',
 	released: true,
 };
